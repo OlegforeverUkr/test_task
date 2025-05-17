e03ced43e066bed9bd4009b2ea33bc35d69005b9
Oleg <shlyapin8848@gmail.com>
2025-05-17 16:23:00 +0300

    fix models

--- api/models.py
+++ api/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 
 class Country(models.Model):
-    code = models.CharField(max_length=2, primary_key=True)
-    name = models.CharField(max_length=100)
-    official_name = models.CharField(max_length=200)
+    code = models.CharField(max_length=5, primary_key=True)
+    name = models.CharField(max_length=200)
+    official_name = models.CharField(max_length=500)
     region = models.CharField(max_length=100)
     subregion = models.CharField(max_length=100)
     independent = models.BooleanField(null=True)
-    google_maps_url = models.URLField(null=True, blank=True)
-    openstreetmap_url = models.URLField(null=True, blank=True)
+    google_maps_url = models.URLField(max_length=500, null=True, blank=True)
+    openstreetmap_url = models.URLField(max_length=500, null=True, blank=True)
     capital_name = models.CharField(max_length=100, null=True, blank=True)
     capital_latitude = models.FloatField(null=True, blank=True)
     capital_longitude = models.FloatField(null=True, blank=True)
-    flag_png_url = models.URLField(null=True, blank=True)
-    flag_svg_url = models.URLField(null=True, blank=True)
-    flag_alt = models.CharField(max_length=200, null=True, blank=True)
-    coat_of_arms_png_url = models.URLField(null=True, blank=True)
-    coat_of_arms_svg_url = models.URLField(null=True, blank=True)
+    flag_png_url = models.URLField(max_length=500, null=True, blank=True)
+    flag_svg_url = models.URLField(max_length=500, null=True, blank=True)
+    flag_alt = models.CharField(max_length=1000, null=True, blank=True)
+    coat_of_arms_png_url = models.URLField(max_length=500, null=True, blank=True)
+    coat_of_arms_svg_url = models.URLField(max_length=500, null=True, blank=True)
     borders = models.ManyToManyField("self", symmetrical=True, blank=True)
 
     class Meta:
